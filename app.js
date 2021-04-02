5dd28faec7d8a712afb5f7a76468f25682dbd2ab
Huseyin Altikulaç <huseyinaltikulac4@gmail.com>
2021-04-02 18:19:31 +0300

    First

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app=express();
 
 app.use(express.static(__dirname + "/public/"));; // for serving the HTML file
 
-
+app.get("/",(req,res,next)=>{
+    res.render("index.html")
+})
 let port = process.env.PORT || 3000
 var server_host = process.env.HOST || '0.0.0.0';
 app.listen(port, server_host, (err) => {
